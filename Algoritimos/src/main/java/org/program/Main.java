f6c4ca44bd09ae3eb4951dcd9a7788568b855fba
Julio Cesar da Silva <juuliocesa2001@gmail.com>
2024-08-21 19:37:25 -0300

    twoSums solucao

---
+++ Algoritimos/src/main/java/org/program/Main.java
@@ -0,0 +1,10 @@
+package org.program;
+
+import static org.program.TwoSums.twoSum;
+
+public class Main {
+    public static void main(String[] args) {
+       int[] arr = {2,7,11,15};
+       twoSum(arr, 9);
+    }
+}
